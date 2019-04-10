b4058c635b01dfdcdc3ff54b974df3b8953a7bab
Lillian Lee <lillian.lee@ssa.gov>
2019-04-10 11:25:50 -0400

    add passwing data from parent to child with @input

--- my-angular-albums/src/app/app.component.ts
+++ my-angular-albums/src/app/app.component.ts
@@ -1,21 +1,14 @@
-import {
-  Component,
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  OnInit
-} from "@angular/core";
+import { Component } from "@angular/core";
 
 import { Album } from "./albums/album.model";
-import { ALBUMS } from "./albums/albums.data";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
-  // Un-comment to prevent UI from updating
-  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = "My Angular Albums";
 
+  constructor() {}
 }
