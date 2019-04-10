86bb870f249be99e3e13b4b98b07d6439163030c
Lillian Lee <lillian.lee@ssa.gov>
2019-04-10 12:19:41 -0400

    add event binding

--- my-angular-albums/src/app/albums/album-card/album-card.component.ts
+++ my-angular-albums/src/app/albums/album-card/album-card.component.ts
@@ -10,6 +10,18 @@ export class AlbumCardComponent implements OnInit {
   @Input()
   album: Album;
 
+  // @Output()
+  //   albumClicked: EventEmitter<Album> = new EventEmitter<Album>();
+
+  showAlbum() {
+    alert("Album selected: " + this.album.album_name);
+    // this.albumClicked.emit(this.album);
+  }
+
+  showLog() {
+    console.log(this.album.album_name)
+  }
+
   constructor() {}
 
   ngOnInit() {}
