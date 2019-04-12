1d03fd7cb6863afd6ba3f86a05aa997ecded46a1
Lillian Lee <lillian.lee@ssa.gov>
2019-04-12 10:33:00 -0400

    add router

--- my-angular-albums/src/app/app.module.ts
+++ my-angular-albums/src/app/app.module.ts
@@ -1,27 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AlbumListComponent } from './albums/album-list/album-list.component';
-import { AlbumCardComponent } from './albums/album-card/album-card.component';
-import { AlbumService } from './albums/album.service';
-import { AlbumDetailsComponent } from './albums/album-details/album-details.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AlbumListComponent } from "./albums/album-list/album-list.component";
+import { AlbumCardComponent } from "./albums/album-card/album-card.component";
+import { AlbumService } from "./albums/album.service";
+import { AlbumDetailsComponent } from "./albums/album-details/album-details.component";
+import { AboutComponent } from "./about/about.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlbumListComponent,
     AlbumCardComponent,
-    AlbumDetailsComponent
+    AlbumDetailsComponent,
+    AboutComponent,
+    NavbarComponent,
+    NotfoundComponent
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [AlbumService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
